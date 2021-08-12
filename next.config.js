2367ef0aac8c19221c5d5a53b3f533550ad79697
Reid Barber <reid@reidbarber.com>
2021-08-12 17:56:56 -0500

    upgrade to Next 11

--- next.config.js
+++ next.config.js
@@ -1,11 +1,46 @@
-const withPlugins = require('next-compose-plugins')
-const withCSS = require('@zeit/next-css')
-const withTM = require('next-transpile-modules')([
-  '@adobe/react-spectrum',
-  '@spectrum-icons/.*',
-  '@react-spectrum/.*'
-])
+const withTM = require("next-transpile-modules")([
+  "@adobe/react-spectrum",
+  "@react-spectrum/actiongroup",
+  "@react-spectrum/breadcrumbs",
+  "@react-spectrum/button",
+  "@react-spectrum/buttongroup",
+  "@react-spectrum/checkbox",
+  "@react-spectrum/combobox",
+  "@react-spectrum/dialog",
+  "@react-spectrum/divider",
+  "@react-spectrum/form",
+  "@react-spectrum/icon",
+  "@react-spectrum/illustratedmessage",
+  "@react-spectrum/image",
+  "@react-spectrum/label",
+  "@react-spectrum/layout",
+  "@react-spectrum/link",
+  "@react-spectrum/listbox",
+  "@react-spectrum/menu",
+  "@react-spectrum/meter",
+  "@react-spectrum/numberfield",
+  "@react-spectrum/overlays",
+  "@react-spectrum/picker",
+  "@react-spectrum/progress",
+  "@react-spectrum/provider",
+  "@react-spectrum/radio",
+  "@react-spectrum/slider",
+  "@react-spectrum/searchfield",
+  "@react-spectrum/statuslight",
+  "@react-spectrum/switch",
+  "@react-spectrum/tabs",
+  "@react-spectrum/text",
+  "@react-spectrum/textfield",
+  "@react-spectrum/theme-dark",
+  "@react-spectrum/theme-default",
+  "@react-spectrum/theme-light",
+  "@react-spectrum/tooltip",
+  "@react-spectrum/view",
+  "@react-spectrum/well",
+  "@spectrum-icons/ui",
+  "@spectrum-icons/workflow"
+]);
 
-module.exports = withPlugins([withCSS, withTM], {
-  // Your next configuration
-})
+module.exports = withTM({
+  // Your Next.js configuration
+});
